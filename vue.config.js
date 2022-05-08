fbf3432b0ba8da31d26c85dd39f15f507e2c2a40
hsiang119 <g8367ay36@gmail.com>
2022-05-08 14:59:56 +0800

    feat: update dist folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  filenameHashing: false
+  filenameHashing: false,
+  publicPath: '/lucky_wheel/dist/'
 })
